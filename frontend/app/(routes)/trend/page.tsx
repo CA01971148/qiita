9c2e844179246532401f2e72d608e002daadfd15
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2024-09-30 13:03:44 +0900

    trend.tsxに基本的なコードを追加

---
+++ frontend/app/(routes)/trend/page.tsx
@@ -0,0 +1,13 @@
+import Card from "@/app/_components/card";
+import Header from "@/app/_components/Header";
+import React from "react";
+
+export default function Trend() {
+  return (
+    <>
+      <Header />
+      <Card />
+      <Card />
+    </>
+  );
+}
