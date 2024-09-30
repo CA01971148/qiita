1f608181f329d7120288b65ea8cd483fe50682f3
CA01971164 <107534471+CA01971164@users.noreply.github.com>
2024-09-30 10:31:50 +0900

    長文消した　かつ　ユーザー名に変更

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -15,18 +15,13 @@ export default function Home() {
               alt="Avatar of Jonathan Reinink"
             />
             <div className="text-sm">
-              <p className="text-gray-900 leading-none">Jonathan Reinink</p>
+              <p className="text-gray-900 leading-none">ユーザー名</p>
               <p className="text-gray-600">Aug 18</p>
             </div>
           </div>
-          <div className="text-gray-900 font-bold text-xl mb-2">
+          <div className="px-6 pt-4 pb-2 text-gray-900 font-bold text-xl mb-2">
             Can coffee make you a better developer?
           </div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Voluptatibus quia, nulla! Maiores et perferendis eaque,
-            exercitationem praesentium nihil.
-          </p>
           <div className="px-6 pt-4 pb-2">
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #photography
@@ -37,9 +32,11 @@ export default function Home() {
             <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
               #winter
             </span>
+            <p>
+              <span className="i-heroicons-solid-heart w-10 h-10 "></span>
+              <span className="i-heroicons-solid-heart w-10 h-10 text-red-500"></span>
+            </p>
           </div>
-          <span className="i-heroicons-solid-heart w-10 h-10 "></span>
-          <span className="i-heroicons-solid-heart w-10 h-10 text-red-500"></span>
         </div>
       </div>
     </div>
