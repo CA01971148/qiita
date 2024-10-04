10e74f6ac64f91c23613fe305e3de5228cacb9d7
CA01971148 <CA01971148@st.kawahara.ac.jp>
2024-10-04 11:20:35 +0900

    test5

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,18 @@
 import Header from "./components/Header";
 import Homepage from "./components/Homepage";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
+    <>
     <div>
       <Header />
       <main>
-        <Homepage />
+      <Homepage />
       </main>
     </div>
+        <Footer />
+    </>
+    
   );
 }
